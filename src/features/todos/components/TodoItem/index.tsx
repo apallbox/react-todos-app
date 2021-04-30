cec6febf0e2885b09d2a97c81a1e1fe5c9f3bbf7
Dmitriy Mamaev <apallbox@gmail.com>
2021-05-01 01:45:08 +0300

    Implement todo status changing

--- src/features/todos/components/TodoItem/index.tsx
+++ src/features/todos/components/TodoItem/index.tsx
@@ -1,15 +1,37 @@
 import './index.css';
 
-import React from 'react';
-import { Todo } from '../../models';
+import React, { MouseEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { Todo, TodoStatus } from '../../models';
+import { todoUpdated } from '../../todosSlice';
 
 interface TodoItemProps {
   todo: Todo;
 }
 
 export default function TodoItem(props: TodoItemProps) {
+  const dispatch = useDispatch();
+
+  const onStatusClicked = (event: MouseEvent) => {
+    const todo = {
+      id: props.todo.id,
+      text: props.todo.text,
+      status: props.todo.status === TodoStatus.Done
+        ? TodoStatus.None
+        : TodoStatus.Done,
+    };
+    dispatch(todoUpdated(todo));
+  };
+
   return (
     <div className="todo-item">
+      <div className="todo-item__status">
+        <input
+          type="checkbox"
+          checked={props.todo.status === TodoStatus.Done}
+          onClick={onStatusClicked}
+        />
+      </div>
       <div className="todo-item__text">
         {props.todo.text}
       </div>
