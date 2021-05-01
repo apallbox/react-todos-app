6769494fecbd08d0810fa86841f38384f637b721
Dmitriy Mamaev <apallbox@gmail.com>
2021-05-01 17:29:23 +0300

    Add extra reducers and actions to handle todo use cases

--- src/features/todos/components/TodoItem/index.tsx
+++ src/features/todos/components/TodoItem/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import React, { ChangeEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import { Todo, TodoStatus } from '../../entities';
-import { todoUpdated } from '../../todosSlice';
+import { todoToggledDone, todoUpdated } from '../../todosSlice';
 import { TodoInteractor } from '../../interactors';
 
 interface TodoItemProps {
@@ -16,9 +16,7 @@ export default function TodoItem(props: TodoItemProps) {
   const dispatch = useDispatch();
 
   const onStatusChanged = (event: ChangeEvent<HTMLInputElement>) => {
-    const todoInteractor = new TodoInteractor({ ...props.todo });
-    todoInteractor.toggleDone();
-    dispatch(todoUpdated(todoInteractor.todo));
+    dispatch(todoToggledDone(props.todo.id));
   };
 
   return (
--- src/features/todos/todosSlice.ts
+++ src/features/todos/todosSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { Todo } from './entities';
+import { TodoInteractor } from './interactors';
 
 const initialState: Todo[] = [];
 
@@ -14,14 +15,67 @@ const todosSlice = createSlice({
     todoUpdated(state, action: PayloadAction<Todo>) {
       const { id, text, status } = action.payload;
       const existingTodo = state.find((todo) => todo.id === id);
-      if (existingTodo) {
-        existingTodo.text = text;
-        existingTodo.status = status;
-      }
+
+      if (!existingTodo) { return; }
+
+      existingTodo.text = text;
+      existingTodo.status = status;
+    },
+    todoRemoved(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const existingTodoIndex = state.findIndex((todo) => todo.id === id);
+
+      if (existingTodoIndex < 0) { return; }
+
+      state.splice(existingTodoIndex, 1);
+    },
+    todoMarkedNew(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const existingTodo = state.find((todo) => todo.id === id);
+
+      if (!existingTodo) { return; }
+
+      const interactor = new TodoInteractor(existingTodo);
+      interactor.markNone();
+    },
+    todoMarkedDone(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const existingTodo = state.find((todo) => todo.id === id);
+
+      if (!existingTodo) { return; }
+
+      const interactor = new TodoInteractor(existingTodo);
+      interactor.markDone();
+    },
+    todoMarkedCancelled(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const existingTodo = state.find((todo) => todo.id === id);
+
+      if (!existingTodo) { return; }
+
+      const interactor = new TodoInteractor(existingTodo);
+      interactor.markCancelled();
+    },
+    todoToggledDone(state, action: PayloadAction<string>) {
+      const id = action.payload;
+      const existingTodo = state.find((todo) => todo.id === id);
+
+      if (!existingTodo) { return; }
+
+      const interactor = new TodoInteractor(existingTodo);
+      interactor.toggleDone();
     },
   },
 });
 
-export const { todoAdded, todoUpdated } = todosSlice.actions;
+export const {
+  todoAdded,
+  todoUpdated,
+  todoRemoved,
+  todoMarkedNew,
+  todoMarkedDone,
+  todoMarkedCancelled,
+  todoToggledDone,
+} = todosSlice.actions;
 
 export default todosSlice.reducer;
